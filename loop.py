793089351ea37f566fdd7afeda9dfa7fcb88bcbb
Ubuntu <ubuntu@ip-172-31-41-46.ec2.internal>
2022-06-17 18:17:31 +0000

    Added File

---
+++ loop.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+# Created by Ethan Prieur
+# Created on June 2022
+# This program displays every integer from 1000-2000
+
+
+def main():
+    # This Is The Main Function
+
+    print("This program displays every integer from 1000 to 2000.")
+
+    loop_counter = 0
+
+    # Process
+    for loop_counter in range(1000, 2000 + 1):
+        if loop_counter % 5 == 0:
+            print("{0}  ".format(loop_counter))
+        else:
+            print("{0}  ".format(loop_counter), end="")
+
+
+if __name__ == "__main__":
+    main()
